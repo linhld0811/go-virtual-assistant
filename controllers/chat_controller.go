6ab7e9f277ffdb352d8612e30157330267a05e0e
linhld152179 <39935721+linhld0811@users.noreply.github.com>
2024-11-28 01:43:54 +0700

    Create chat_controller.go

---
+++ controllers/chat_controller.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+	"net/http"
+	"virtual-assistant/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+type ChatRequest struct {
+	Query string `json:"query"`
+}
+
+func HandleChat(c *gin.Context) {
+	var request ChatRequest
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	response, err := services.ChatbotService(request.Query)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"response": response})
+}
